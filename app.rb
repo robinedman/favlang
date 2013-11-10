5021b8a1a07960ffb5adba20a5ef455271bcb1f7
robin <robin.edman@gmail.com>
2013-11-10 19:44:59 +0100

    Specify json response

--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
 require 'sinatra'
+require 'json'
 require_relative 'favourite_language_guesser'
 
 language_guesser = FavouriteLanguageGuesser.new
 
 get '/' do
-  erb :index
+  send_file('./views/index.html')
 end
 
 # User's favourite programming language
 get '/:username/favourite' do |username|
+  content_type :json
+
   {
     favourite: language_guesser.favourite_language(username),
     username: username
