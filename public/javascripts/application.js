a92be7ae2cb92dfdab58af38a2a7b9069d128565
robin <robin.edman@gmail.com>
2013-11-11 15:07:45 +0100

    Don't make a request when the form is empty.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,18 +2,21 @@ $(document).ready(function() {
   $("#guessing-form").submit(function(e) {
     e.preventDefault();
     var username = $("#username").val();
-    $.get(username + "/favourite")
-      .success(function(answer) {
-        $("#answer").html(answer.username + "'s favourite language seems to be <strong>" + answer.favourite + "</strong>")
-                    .removeClass('hidden')
-                    .addClass('alert-success')
-                    .removeClass('alert-danger');
-      })
-      .error(function(err) {
-        $("#answer").text(err.responseText)
-                    .removeClass('hidden')
-                    .addClass('alert-danger')
-                    .removeClass('alert-success');
-      });
+    if (username) {
+      $.get(username + "/favourite")
+        .success(function(answer) {
+          $("#answer").html(answer.username + "'s favourite language seems to be <strong>" + answer.favourite + "</strong>")
+                      .removeClass('hidden')
+                      .addClass('alert-success')
+                      .removeClass('alert-danger');
+        })
+        .error(function(err) {
+          $("#answer").text(err.responseText)
+                      .removeClass('hidden')
+                      .addClass('alert-danger')
+                      .removeClass('alert-success');
+        });    
+    };
+    
   }); 
 });
